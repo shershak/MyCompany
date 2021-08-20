65aa75efaa9c81da6af7c172a9b29150e2f08b59
Oleksandr Shershak <scherschak@gmail.com>
2021-08-20 18:45:58 +0300

    Create base functionality by video

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCompany.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DataManager dataManager;
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+        public IActionResult Index()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
+        }
+    }
+}
